ab438ae7207f086f79e1648c20a42eb1f309e4fd
inhauni <inhauni123@gmail.com>
2023-02-06 09:45:55 +0900

    toString override

---
+++ src/edu/network/Network_JAVA.java
@@ -0,0 +1,15 @@
+package edu.network;
+
+public class Network_JAVA {
+
+	public static void main(String[] args) {
+		
+		Node node = new Node();
+		node.data="피카츄";
+		System.out.println(node);
+				
+	
+
+	}
+
+}
