3fdd891033c23ecf2ae819227465a3ba276c97ba
inhauni <inhauni123@gmail.com>
2023-02-13 13:03:30 +0900

    network 2

--- src/edu/network/Day21.java
+++ src/edu/network/Day21.java
@@ -1,23 +1,36 @@
 package edu.network;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+//import java.net.UnknownHostException;
 
 public class Day21 {
 
 	public static void main(String[] args) {
-		try {
-			InetAddress addr = InetAddress.getByName("www.inha.ac.kr");
-//			InetAddress addr = InetAddress.getByName("www.kiminha.ac.kr"); //UnknownHostException 발생
-//			System.out.println(addr);
-			System.out.println(addr.getCanonicalHostName());
-			System.out.println(addr.getHostAddress());
-		} catch (UnknownHostException e) {  // python 의 exception 구문과 같은 역할
+		 try {         
+			 URL url = new URL("http://www.daum.net");  // if 잘못된 주소이면 connection이 만들어지지 않음         
+			 URLConnection urlConnection = url.openConnection();  // 주소와 연결 생성
+			 BufferedReader br = new BufferedReader(       //getInputStream() : connection된 주소와의 stream 생성          
+				 new InputStreamReader(urlConnection.getInputStream()) /*inputstreamreader : 웹 브라우저의 html코드를 텍스트 형태로 담아둠 
+				 														-> bufferedreader로 다시 임시저장공간에 저장*/
+			);         // decorator 2개와 같은 형식
+			 String line;        
+			 while ((line = br.readLine()) != null) {     // br을 한줄씩 line에 넣어서 null이 나오기 전까지 한줄씩 출력        
+				 System.out.println(line);         
+			 	}         
+			 br.close();     
+		 } 
+		 
+		 catch (IOException ex) {         // Handle exceptions     }
+
 			
-//			e.printStackTrace();
-			System.out.println("해당 url은 존재하지 않습니다.");
+
 		}
 
 	}
-
+			
 }
