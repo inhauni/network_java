d928aa733110e0bdf3f4b3c701bf07ce5e498483
inhauni <inhauni123@gmail.com>
2023-02-15 15:27:54 +0900

    ìMakingNew Object without Constructor

--- src/test.java
+++ src/test.java
@@ -3,49 +3,26 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+class One{
+    private One(){
+
+    }
+
+    public static One produceOneInstance(){  // return type = One 객체
+        return new One();   // 생성자를 호출하는 코드
+    }
+
+}
 public class test {
 
-    public static String getMACIdentifier(NetworkInterface network) {
-        StringBuilder identifier = new StringBuilder();
-
-        try {
-            byte[] macBuffer = network.getHardwareAddress();
-
-            if (macBuffer != null) {
-                for (int i = 0; i < macBuffer.length; i++) {   // 02X => 16진수
-                    identifier.append(String.format("%02X%s",macBuffer[i],(i < macBuffer.length - 1) ? ":" : ""));
-                }
-            }
-            else {
-                return "---";
-            }
-        }
-        catch (SocketException ex) {
-            ex.printStackTrace();
-        }
-        return identifier.toString();
-
-
-    }
 
     public static void main(String[] args) {
+//        One o = new One();
 
-
-        InetAddress address = null;
-        try {
-            address = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("IP address: " + address.getHostAddress());
-        NetworkInterface network = null;
-        try {
-            network = NetworkInterface.getByInetAddress(address);
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("MAC address: " + getMACIdentifier(network));
-
+        One o = One.produceOneInstance(); // 꼭 생성자만이 새 객체를 만들 수 있는 것은 아니다
     }
+
+
+
 
 }
